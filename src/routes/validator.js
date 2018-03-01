2e4a24293b366c59ea3afa05bbca2ce88f5a4b17
luma <stubma@gmail.com>
2018-03-01 13:05:25 +0800

    support isValid args

--- src/routes/validator.js
+++ src/routes/validator.js
@@ -8,6 +8,7 @@ import { ErrCode } from '../models'
  {
  	paramName: {
  		isValid: function to validate parameter,
+ 		args: arguments for isValid function,
  		optional: true/false
  	},
  	... more
@@ -45,10 +46,16 @@ function validateRequestParams() {
 						valid = false
 						ErrCode.build(ctx, ErrCode.ERR_PARAM_MISSING, key)
 						break
-					} else if(param && !validator.isValid(param)) {
-						valid = false
-						ErrCode.build(ctx, ErrCode.ERR_PARAM_NOT_VALID, key)
-						break
+					} else if(param) {
+						if(validator.args) {
+							valid = validator.isValid(param, ...validator.args)
+						} else {
+							valid = validator.isValid(param)
+						}
+						if(!valid) {
+							ErrCode.build(ctx, ErrCode.ERR_PARAM_NOT_VALID, key)
+							break
+						}
 					}
 				}
 
@@ -71,15 +78,57 @@ function positiveNumberValidator(value) {
 }
 
 /**
+ * validator which ensures value is a number and in a range
+ * @param value value
+ * @param min min value
+ * @param max max value
+ * @returns {boolean}
+ */
+function rangeNumberValidator(value, min, max) {
+	if(!_.isNumber(value)) {
+		return false
+	}
+	if(min != undefined && value < min) {
+		return false
+	}
+	if(max != undefined && value > max) {
+		return false
+	}
+	return true
+}
+
+/**
  * validator which ensures value is a non-empty string
  */
 function nonEmptyStringValidator(value) {
 	return _.isString(value) && value.length > 0
 }
 
+/**
+ * validator which ensures value is string and has proper length
+ * @param value value
+ * @param minLength minimum length
+ * @param maxLength maximum length
+ * @returns {boolean}
+ */
+function lengthStringValidator(value, minLength, maxLength) {
+	if(!_.isString(value)) {
+		return false
+	}
+	if(minLength != undefined && value.length < minLength) {
+		return false
+	}
+	if(maxLength != undefined && value.length > maxLength) {
+		return false
+	}
+	return true
+}
+
 export default {
 	registerParamSchema,
 	validateRequestParams,
 	positiveNumberValidator,
-	nonEmptyStringValidator
+	rangeNumberValidator,
+	nonEmptyStringValidator,
+	lengthStringValidator
 }
